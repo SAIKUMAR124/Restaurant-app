6b2d2a5419ee390c4f1550a8af668434085e36e9
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-06 11:47:57 +0530

    Restructure of Restaurent completed

--- src/pages/restaurents/Restaurents.js
+++ src/pages/restaurents/Restaurents.js
@@ -1,46 +1,24 @@
 import React from "react";
-import { RestRetriveData } from "./rest-components/RestRetriveData";
+import RestaurentItemCart from "./rest-components/RestaurentItemCart";
+import RestaurentItemNav from "./rest-components/RestaurentItemNav";
+import RestaurentItems from "./rest-components/RestaurentItems";
 import "./Restaurents.css";
 import RestHead from "./RestHead";
 
 const Restaurents = () => {
-  const d = RestRetriveData();
-  const { data } = d;
-  const { food } = data;
-
   return (
     <div>
       <RestHead />
-      <div className="rest-main">
-        {food.map((b, index) => {
-          const { type, food_item } = b;
-          const type_line = index > 0 ? "type-underline" : "";
-
-          return (
-            <div key={index}>
-              <div className={type_line}></div>
-              <div className="rest-type-head">
-                <h1>{type}</h1>
-                <p className="rest-type-items">{food_item.length} ITEMS</p>
-              </div>
-              {food_item.map((a, index) => {
-                const { item, price, item_info } = a;
-                const l = food_item.length;
-
-                const border_line = index !== l - 1 ? "food-underline" : "";
-                return (
-                  <div key={index} className="food-info">
-                    <div className="food-item-name">{item}</div>
-                    <div className="food-item-price">{price}</div>
-                    <div className="food-item-text">{item_info}</div>
-
-                    <div className={border_line}></div>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+      <div className="Restaurent-parts">
+        <div className="Restaurent-part1">
+          <RestaurentItemNav />
+        </div>
+        <div className="Restaurent-part2">
+          <RestaurentItems />
+        </div>
+        <div className="Restaurent-part3">
+          <RestaurentItemCart />
+        </div>
       </div>
     </div>
   );
--- src/pages/restaurents/rest-components/RestaurentItems.js
+++ src/pages/restaurents/rest-components/RestaurentItems.js
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from "react";
+import { RestRetriveData } from "./RestRetriveData";
 
 const RestaurentItems = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const d = RestRetriveData();
+  const { data } = d;
+  const { food } = data;
 
-export default RestaurentItems
+  return (
+    <div>
+      <div className="rest-main">
+        {food.map((b, index) => {
+          const { type, food_item } = b;
+          const type_line = index > 0 ? "type-underline" : "";
+
+          return (
+            <div key={index}>
+              <div className={type_line}></div>
+              <div className="rest-type-head">
+                <h1>{type}</h1>
+                <p className="rest-type-items">{food_item.length} ITEMS</p>
+              </div>
+              {food_item.map((a, index) => {
+                const { item, price, item_info } = a;
+                const l = food_item.length;
+
+                const border_line = index !== l - 1 ? "food-underline" : "";
+                return (
+                  <div key={index} className="food-info">
+                    <div className="food-item-name">{item}</div>
+                    <div className="food-item-price">{price}</div>
+                    <div className="food-item-text">{item_info}</div>
+
+                    <div className={border_line}></div>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default RestaurentItems;
