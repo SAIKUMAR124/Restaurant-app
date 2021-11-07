92e8bb4787ba3aa78f1087e065178d0c45d98197
SAIKUMAR124 <pavankumar.s12459@gmail.com>
2021-11-07 14:07:06 +0530

    added Validation to Address

--- src/pages/CheckOut.js
+++ src/pages/CheckOut.js
@@ -5,9 +5,38 @@ import "./CheckOut.css";
 import { useGlobalLoginContext } from "../context/LoginContext";
 import { Redirect } from "react-router";
 
+const validate = (values) => {
+  const errors = {};
+  if (!values.doorNo) {
+    errors.doorNo = "Required";
+  } else if (values.doorNo.length > 20) {
+    errors.doorNo = "Must be 20 characters or less";
+  }
+
+  if (!values.lankMark) {
+    errors.lankMark = "Required";
+  } else if (values.lankMark.length < 15) {
+    errors.lankMark = "Must be 15 characters";
+  }
+
+  if (!values.village) {
+    errors.village = "Required";
+  } else if (values.village.length < 15) {
+    errors.village = "Must be 15 characters";
+  }
+
+  if (!values.pinCode) {
+    errors.pinCode = "Required";
+  } else if (values.pinCode.length !== 6) {
+    errors.pinCode = "Must be 6 characters";
+  }
+
+  return errors;
+};
+
 const CheckOut = () => {
   const { handleAddress } = useGlobalLoginContext();
-    const [addressSubmit, setAddressSubmit] = useState(false);
+  const [addressSubmit, setAddressSubmit] = useState(false);
 
   const formik = useFormik({
     initialValues: {
@@ -16,6 +45,7 @@ const CheckOut = () => {
       village: "",
       pinCode: "",
     },
+    validate,
     onSubmit: (values) => {
       const newItems = {
         doorNo: values.doorNo,
@@ -40,6 +70,8 @@ const CheckOut = () => {
             placeholder="Door No.."
             {...formik.getFieldProps("doorNo")}
           />
+          {formik.touched.doorNo && formik.errors.doorNo ? <div>{formik.errors.doorNo}</div> : null}
+
           <input
             type="text"
             id="lankMark"
@@ -47,6 +79,8 @@ const CheckOut = () => {
             placeholder="Landmark"
             {...formik.getFieldProps("lankMark")}
           />
+          {formik.touched.lankMark && formik.errors.lankMark ? <div>{formik.errors.lankMark}</div> : null}
+
           <input
             type="text"
             id="village"
@@ -54,6 +88,8 @@ const CheckOut = () => {
             placeholder="Village"
             {...formik.getFieldProps("village")}
           />
+          {formik.touched.village && formik.errors.village ? <div>{formik.errors.village}</div> : null}
+
           <input
             type="text"
             id="pinCode"
@@ -61,10 +97,12 @@ const CheckOut = () => {
             placeholder="Pin Code"
             {...formik.getFieldProps("pinCode")}
           />
+          {formik.touched.pinCode && formik.errors.pinCode ? <div>{formik.errors.pinCode}</div> : null}
+
           <button className="checkout-submit" type="submit">
             Submit
           </button>
-          {addressSubmit===true && <Redirect to='/success' />}
+          {addressSubmit === true && formik.errors && <Redirect to="/success" />}
         </form>
       </div>
       <div className="checkout-part2">
